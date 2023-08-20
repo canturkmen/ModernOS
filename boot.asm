32dcf3eaa43d7accc2414ed14f84580c408f0642
Mehmet Can Turkmen <turkmenc098@gmail.com>
2023-08-20 15:44:29 +0300

    Switched to protected mode

--- boot.asm
+++ boot.asm
@@ -1,5 +1,8 @@
-ORG 0
+ORG 0x7c00
 BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 _start:
     jmp short start
@@ -8,58 +11,67 @@ _start:
 times 33 db 
 
 start:
-    jmp 0x7c0:step2
+    jmp 0:step2
 
 step2:
     cli ; Clear Interrupts
-    mov ax, 0x7c0
+    mov ax, 0x00
     mov ds, ax
     mov es, ax
-
-    mov ax, 0x00
     mov ss, ax
     mov sp, 0x7c00     
+    sti ; Enables Interrupts           
 
-    sti ; Enables Interrupts
+.load_protected:
+    cli
+    lgdt [gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
 
-    mov ah, 2 ; READ SECTOR COMMAND
-    mov al, 1 ; ONE SECTOR TO READ
-    mov ch, 0 ; Cylinder low eight bits
-    mov cl, 2 ; Read sector two
-    mov dh, 0 ; Head number
-    mov bx, buffer
-    int 0x13
-    jc error
-    mov si, buffer
-    call print
-    jmp $
+; GDT
+gdt_start:
 
-error:
-    mov si, error_message
-    call print
-    jmp $
+gdt_null:
+    dd 0x0
+    dd 0x0
 
-print:
-    mov bx, 0
+; offset 0x8
+gdt_code:       ; CS should point to this 
+    dw 0xffff   ; Segment limit first 0-15 bits
+    dw 0        ; Base first 0-15 bits
+    db 0        ; Base 16-23 bits
+    db 0x9a     ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-32 bits  
 
-.loop:
-    lodsb
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
+; offset 0x10
+gdt_data:       ; DS, SS, ES, FS, GS
+    dw 0xffff   ; Segment limit first 0-15 bits
+    dw 0        ; Base first 0-15 bits
+    db 0        ; Base 16-23 bits
+    db 0x92     ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-32 bits  
 
-.done:
-    ret
+gdt_end:
 
-print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
 
-error_message: db 'Failed to load sector', 0
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
+    jmp $  
 
 times 510-($ - $$) db 0
 dw 0xAA55
-
-buffer:
