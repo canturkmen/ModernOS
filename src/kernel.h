a4cdf54c59f8d9d423d9eb9eaa4d1f57d5ca1211
Mehmet Can Turkmen <turkmenc098@gmail.com>
2023-09-18 15:53:37 +0300

    Implemented a kernel panic

--- src/kernel.h
+++ src/kernel.h
@@ -5,12 +5,13 @@
 #define VGA_HEIGHT 20
 
 #define MODERNOS_MAX_PATH 108
-
+     
 #define ERROR(value) (void*)value
 #define ERROR_I(value) (int)value
 #define ISERR(value) ((int)value) < 0
 
 void kernel_main();
 void print(const char* str);
+void panic(const char* message);
 
 #endif 
