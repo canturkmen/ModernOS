7ebd75e638c0a9cb4bb98813888c9b9929c16096
Mehmet Can Turkmen <turkmenc098@gmail.com>
2023-09-20 23:00:14 +0300

    Implemented Task Switch Segment

--- src/kernel.c
+++ src/kernel.c
@@ -11,6 +11,7 @@
 #include "disk/streamer.h"
 #include "fs/pparser.h"
 #include "gdt/gdt.h"
+#include "task/tss.h"
 
 #include <stdint.h>
 #include <stddef.h>
@@ -79,11 +80,15 @@ void panic(const char* message)
     }
 }
 
+struct tss tss;
 struct gdt gdt_real[MODERNOS_TOTAL_GDT_SEGMENTS];
 struct gdt_structured gdt_structed[MODERNOS_TOTAL_GDT_SEGMENTS] = {
-    {.base = 0x00, .limit = 0x00, .type = 0x00},            // NULL Segment
-    {.base = 0x00, .limit = 0xFFFFFFFF, .type = 0x9A},      // Kernel Code Segment
-    {.base = 0x00, .limit = 0xFFFFFFFF, .type = 0x92}       // Kernel Data Segment
+    {.base = 0x00, .limit = 0x00, .type = 0x00},                        // NULL Segment
+    {.base = 0x00, .limit = 0xFFFFFFFF, .type = 0x9A},                  // Kernel Code Segment
+    {.base = 0x00, .limit = 0xFFFFFFFF, .type = 0x92},                  // Kernel Data Segment
+    {.base = 0x00, .limit = 0xFFFFFFFF, .type = 0xF8},                  // User Code Segment
+    {.base = 0x00, .limit = 0xFFFFFFFF, .type = 0xF2},                  // User Data Segment
+    {.base = (uint32_t)&tss, .limit = sizeof(tss), .type = 0xE9}     // TSS Segment
 };
  
 void kernel_main() 
@@ -107,6 +112,14 @@ void kernel_main()
 
     // Initialize the interrupt descriptor table
     idt_init();
+
+    // Setup the tss
+    memset(&tss, 0x00, sizeof(tss));
+    tss.esp0 = 0x600000;
+    tss.ss0 = KERNEL_DATA_SELECTOR;
+
+    // Load the tss
+    tss_load(0x28);
 
     // Setup paging
     kernel_chunk = paging_new_4gb(PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
