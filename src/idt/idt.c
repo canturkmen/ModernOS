b182a5c41763e73f82a00ea828bacb3d2c939f2c
Mehmet Can Turkmen <turkmenc098@gmail.com>
2023-09-23 21:17:29 +0300

    Kernel commands can now be createde and executed

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -3,6 +3,9 @@
 #include "../memory/memory.h"
 #include "../io/io.h"
 #include "kernel.h"
+#include "task/task.h"
+
+static ISR80H_COMMAND isr80h_commands[MODERNOS_MAX_ISR80H_COMMANDS];
 
 struct idt_desc idt_descriptors[MODERNOS_TOTAL_INTERRUPTS];
 struct idtr_desc idtr_descriptor;
@@ -10,6 +13,7 @@ struct idtr_desc idtr_descriptor;
 extern void idt_load(struct idtr_desc* ptr);
 extern void int21h();
 extern void no_interrupt();
+extern void isr80h_wrapper();
 
 void int21h_handler()
 {
@@ -48,14 +52,36 @@ void idt_init()
 
     idt_set(0, idt_zero);
     idt_set(0x21, int21h);
+    idt_set(0x80, isr80h_wrapper);
 
     // Load the interrupt descriptor table
     idt_load(&idtr_descriptor);
 }
 
-void isr80h_handle_command(int command, struct interrupt_frame* frame)
+void isr80h_register_command(int command_id, ISR80H_COMMAND command)
 {
-    
+    if(command_id <= 0 || command_id >= MODERNOS_MAX_ISR80H_COMMANDS)
+        panic("Command is out of bounds\n");
+
+    if(isr80h_commands[command_id])
+        panic("You're attempting to overwrite an existing command\n");
+
+    isr80h_commands[command_id] = command;
+}
+
+void* isr80h_handle_command(int command, struct interrupt_frame* frame)
+{
+    void* result = 0;
+    if(command <= 0 || command >= MODERNOS_MAX_ISR80H_COMMANDS)
+        return 0;
+
+    ISR80H_COMMAND command_func = isr80h_commands[command];
+    if(!command_func)
+        return 0;
+
+    result = command_func(frame);
+
+    return result;
 }
 
 void* isr80h_handler(int command, struct interrupt_frame* frame)
