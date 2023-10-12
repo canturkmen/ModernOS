5766e4b704ebcd775f677e6b4a8b3b5ba0ae9d3c
Mehmet Can Turkmen <turkmenc098@gmail.com>
2023-10-12 18:30:35 +0300

    Implemented printf in stdio

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -2,20 +2,21 @@
 #include "stdlib.h"
 #include "stdio.h"
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
+    printf("My age is %i\n", 21);
     print("Hello, how are you\n");
 
     print(itoa(5343));
     putchar('Z');
 
-    void* ptr = malloc(512);
+    void *ptr = malloc(512);
     free(ptr);
 
-    while(1) 
+    while (1)
     {
-        if(getkey() != 0)
-            print("Key was pressed");    
+        if (getkey() != 0)
+            print("Key was pressed");
     }
     return 0;
 }
--- programs/stdlib/src/modernos.h
+++ programs/stdlib/src/modernos.h
@@ -3,10 +3,10 @@
 
 #include <stddef.h>
 
-void print(const char* message);
+void print(const char *message);
 int getkey();
 void modernos_putchar(char c);
-void* modernos_malloc(size_t size);
-void modernos_free(void* ptr);
+void *modernos_malloc(size_t size);
+void modernos_free(void *ptr);
 
 #endif
